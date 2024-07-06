cdba601679b2c47d27b555abca6b948a8b67d32c
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-07-06 10:53:40 +0530

    setup: project

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Poll Pulse",
+  description: "📊 Voting made easy",
+  icons: [
+    {
+      url: '/poll.png',
+      href: '/poll.png',
+    },
+  ],
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("h-full", inter.className)}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button"
+
+import localFont from "next/font/local"
+import { Poppins } from 'next/font/google'
+import { cn } from "@/lib/utils"
+
+const headingFont = localFont({
+  src: '@/../../../public/fonts/font.woff2',
+})
+
+const textFont = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400'],
+})
+
+export default function Home() {
+  return (
+    <div className={"flex flex-col space-y-3 w-max"}>
+      <span className={cn("text-xl text-blue-700", headingFont.className)}>Hello Poll Pulse</span>
+      <Button>Click me</Button>
+    </div>
+  );
+}
